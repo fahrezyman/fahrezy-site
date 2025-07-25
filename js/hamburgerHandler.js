d628b1576cbce138d035055fd68784f2d5b5f2d6
Ilham Fahrezy <ilhamfahrezyxz@gmail.com>
2025-07-25 17:11:13 +0700

    Changing the menu navlinks behavior to close the navlink if the page is clicked

--- js/hamburgerHandler.js
+++ js/hamburgerHandler.js
@@ -1,19 +1,25 @@
 const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("nav-links");
+const homepage = document.getElementById("home");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 
-document.querySelectorAll(".nav-links a").forEach(link => {
+window.addEventListener("click", (event) => {
+  if (!hamburger.contains(event.target) && !navLinks.contains(event.target)) {
+    navLinks.classList.remove("active");
+  }
+});
+
+document.querySelectorAll(".nav-links a").forEach((link) => {
   link.addEventListener("click", () => {
     navLinks.classList.remove("active");
   });
 });
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   if (window.innerWidth > 768) {
-    navLinks.classList.remove('active');
+    navLinks.classList.remove("active");
   }
 });
-
